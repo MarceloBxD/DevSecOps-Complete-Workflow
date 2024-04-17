fc84e69034ed8f2eb9f20dfa440e45158f989f71
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-04-17 14:32:49 -0300

    feat/resolving calculator issue

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,8 +8,9 @@ public class Calculator {
     public int addition(String arguments) {
 
         int sum = 0;
-        for (String add : arguments.split("\\+"))
+        for (String add : arguments.split("\\+")){
             sum += Integer.valueOf(add);
-            return sum;
+        }
+        return sum;
     }
 }
